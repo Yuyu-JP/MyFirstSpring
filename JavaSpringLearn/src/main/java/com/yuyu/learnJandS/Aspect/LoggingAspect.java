857afc4ded232cd531f951d2d19180d8dd147527
zhaoyuyu <38467453@qq.com>
2024-11-13 09:35:33 +0900

    add AOP to controller

---
+++ JavaSpringLearn/src/main/java/com/yuyu/learnJandS/Aspect/LoggingAspect.java
@@ -0,0 +1,27 @@
+package com.yuyu.learnJandS.Aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    // Pointcut to match all methods in classes annotated with @Controller
+    @Before("within(@org.springframework.stereotype.Controller *)")
+    public void logBefore(JoinPoint joinPoint) {
+        logger.info("Entering method: {} with arguments: {}", joinPoint.getSignature(), joinPoint.getArgs());
+    }
+
+    @AfterReturning(pointcut = "within(@org.springframework.stereotype.Controller *)", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        logger.info("Exiting method: {} with result: {}", joinPoint.getSignature(), result);
+    }
+}
