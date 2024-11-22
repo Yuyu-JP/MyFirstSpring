3462d8053c649ca9fec34893e02b7a66cbbf4d69
zhaoyuyu <38467453@qq.com>
2024-11-22 14:50:32 +0900

    Add exception handler
    add exception handler for other exceptions of get student by ID

--- JavaSpringLearn/src/main/java/com/yuyu/learnJandS/rest/StudentRestController.java
+++ JavaSpringLearn/src/main/java/com/yuyu/learnJandS/rest/StudentRestController.java
@@ -17,41 +17,52 @@ import com.yuyu.learnJandS.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	private StudentDAO studentDAO;
-	
+
 	public StudentRestController(StudentDAO studentDAO) {
 		this.studentDAO = studentDAO;
 	}
-	
-	//define endpoint for "/students" to get student list
+
+	// define endpoint for "/students" to get student list
 	@GetMapping("/students")
 	public List<Student> getStudents() {
-		
+
 		List<Student> students = studentDAO.getAllStudents();
-		
+
 		return students;
 	}
-		
+
 	@GetMapping("/students/{studentId}")
 	public Student getStudentById(@PathVariable int studentId) {
 		Student student = studentDAO.findById(studentId);
-		
+
 		if (student == null) {
 			throw new StudentNotFoundException("The student with ID '" + studentId + "' is not found.");
 		}
-		
+
 		return student;
 	}
-	
+
 	@ExceptionHandler
 	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-		
+
 		StudentErrorResponse error = new StudentErrorResponse();
-		
+
 		error.setStatus(HttpStatus.NOT_FOUND.value());
 		error.setMessage(exc.getMessage());
 		error.setTimeStamp(System.currentTimeMillis());
-		
+
 		return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.NOT_FOUND);
 	}
-	
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+		StudentErrorResponse error = new StudentErrorResponse();
+
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.BAD_REQUEST);
+	}
 }
